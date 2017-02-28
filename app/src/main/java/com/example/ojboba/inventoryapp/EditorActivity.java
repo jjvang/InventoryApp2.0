a65d038c27bc810fea8812f6b555852fee912cbb
jjvang <jjv4ko@gmail.com>
2017-02-27 17:53:00 -0800

    third commit

--- app/src/main/java/com/example/ojboba/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/ojboba/inventoryapp/EditorActivity.java
@@ -1,6 +1,5 @@
 package com.example.ojboba.inventoryapp;
 
-import android.Manifest;
 import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
@@ -9,9 +8,10 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,6 +19,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by OjBoba on 2/22/2017.
@@ -62,6 +67,10 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
     private TextView mQuantityTextView;
 
     private ImageView productImage;
+
+    String mCurrentPhotoPath;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -193,12 +202,12 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         mChooseImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                dispatchTakePictureIntent();
-                ActivityCompat.requestPermissions(
-                        EditorActivity.this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        REQUEST_CODE_GALLERY
-                );
+                dispatchTakePictureIntent();
+//                ActivityCompat.requestPermissions(
+//                        EditorActivity.this,
+//                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+//                        REQUEST_CODE_GALLERY
+//                );
             }
         });
 
@@ -206,11 +215,49 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
     }
 
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the Fi
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     @Override
